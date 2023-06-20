55721815dacd700a6780bba82575c65ba8727441
piratos <sayf.piratos@gmail.com>
2023-06-20 08:47:32 +0200

    Add support for selection text to be sent to API
    Rename dummy naming and add an icon

--- extension/src/extension.ts
+++ extension/src/extension.ts
@@ -2,10 +2,8 @@ import * as vscode from 'vscode';
 import axios from 'axios';
 import WebSocket from 'ws';
 
-let disposable: vscode.Disposable;
-
 export function activate(context: vscode.ExtensionContext) {
-  disposable = vscode.commands.registerCommand('extension.sendLineToApi', async () => {
+  let disposableline = vscode.commands.registerCommand('extension.sendLineToApi', async () => {
     const editor = vscode.window.activeTextEditor;
     if (editor) {
       const line = editor.document.lineAt(editor.selection.active.line).text.trim();
@@ -13,9 +11,9 @@ export function activate(context: vscode.ExtensionContext) {
     }
   });
 
-  context.subscriptions.push(disposable);
+  context.subscriptions.push(disposableline);
 
-  disposable = vscode.commands.registerCommand('extension.sendWordsToApi', async () => {
+  let disposablewords = vscode.commands.registerCommand('extension.sendWordsToApi', async () => {
     const editor = vscode.window.activeTextEditor;
     if (editor) {
       const line = editor.document.lineAt(editor.selection.active.line).text.trim();
@@ -23,57 +21,59 @@ export function activate(context: vscode.ExtensionContext) {
     }
   });
 
-  context.subscriptions.push(disposable);
+  context.subscriptions.push(disposablewords);
+
+  let disposableselection = vscode.commands.registerCommand('extension.sendSelectionToApi', async () => {
+    const editor = vscode.window.activeTextEditor;
+    if (editor) {
+      await sendSelectionToApi();
+    }
+  });
+
+  context.subscriptions.push(disposableselection);
 }
 
-function insertTextAfterCurrentLine2(text: string) {
-  console.log("Insert Text received " + text);
-  const editor = vscode.window.activeTextEditor;
-  if (editor) {
-    const selection = editor.selection;
-    const line = selection.active.line;
-    const lineText = editor.document.lineAt(line).text;
-
-    editor.edit((editBuilder) => {
-      editBuilder.insert(new vscode.Position(line + 1, 0), `${text}\n`);
-    }).then(() => {
-      const newPosition = new vscode.Position(line + 2, 0); // Move the cursor to the line after the pasted text
-      const newSelection = new vscode.Selection(newPosition, newPosition);
-      editor.selection = newSelection;
-    });
-  }
-}
-
-async function insertTextAfterCurrentLine(text: string) {
+async function insertTextAfterCurrentLine(text: string, after_selection: boolean = false) {
   console.log(text);
   const editor = vscode.window.activeTextEditor;
   if (editor) {
-    const line = editor.selection.active.line;
-    const lineText = editor.document.lineAt(line).text;
-
-    await new Promise<void>((resolve) => {
-      editor.edit((editBuilder) => {
-        editBuilder.insert(new vscode.Position(line + 1, 0), `${text}\n`);
-      }).then(() => {
-        resolve();
+    if (after_selection) {
+      const selection = editor.selection;
+      const endPosition = selection.end;
+  
+      await editor.edit((editBuilder) => {
+        editBuilder.insert(endPosition, text);
       });
-    });
+    }
+    else {
+      const line = editor.selection.active.line;
+      const lineText = editor.document.lineAt(line).text;
 
-    const newPosition = new vscode.Position(line + 2, 0); // Move the cursor to the line after the pasted text
-    const newSelection = new vscode.Selection(newPosition, newPosition);
-    editor.selection = newSelection;
+      await new Promise<void>((resolve) => {
+        editor.edit((editBuilder) => {
+          editBuilder.insert(new vscode.Position(line + 1, 0), `${text}\n`);
+        }).then(() => {
+          resolve();
+        });
+      });
+      const newPosition = new vscode.Position(line + 2, 0); // Move the cursor to the line after the pasted text
+      const newSelection = new vscode.Selection(newPosition, newPosition);
+      editor.selection = newSelection;
+    }
+
   }
 }
 
 
 async function sendLineToApi(line: string) {
   const endpoint = vscode.workspace.getConfiguration().get('lineToApi.apiEndpoint') as string;
+  const maxtokens = vscode.workspace.getConfiguration().get('lineToApi.maxTokens') as number;
 
   try {
     const data = {
       inputs: line,
       parameters: {
-        max_new_tokens: 10
+        max_new_tokens: maxtokens
       }
     };
 
@@ -88,8 +88,45 @@ async function sendLineToApi(line: string) {
   }
 }
 
+
+async function sendSelectionToApi() {
+  const editor = vscode.window.activeTextEditor;
+  const endpoint = vscode.workspace.getConfiguration().get('lineToApi.apiEndpoint') as string;
+  const maxtokens = vscode.workspace.getConfiguration().get('lineToApi.maxTokens') as number;
+  if (editor) {
+    const selection = editor.selection;
+    const selectedText = editor.document.getText(selection);
+
+    if (selectedText) {
+      try {
+        const response = await axios.post(endpoint, {
+          inputs: selectedText,
+          parameters: {
+            max_new_tokens: maxtokens
+          }
+        });
+
+        const generatedText = response.data.generated_text;
+        if (generatedText) {
+          await insertTextAfterCurrentLine(generatedText, true);
+        } else {
+          vscode.window.showErrorMessage('Failed to generate text from API.');
+        }
+      } catch (error) {
+        vscode.window.showErrorMessage('An error occurred while calling the API.');
+      }
+    } else {
+      vscode.window.showErrorMessage('No text is selected.');
+    }
+  } else {
+    vscode.window.showErrorMessage('No active text editor.');
+  }
+}
+
+
 async function sendWordsToApi(line: string) {
   const endpoint = vscode.workspace.getConfiguration().get('lineToApi.websocketEndpoint') as string;
+  const maxtokens = vscode.workspace.getConfiguration().get('lineToApi.maxTokens') as number;
   const editor = vscode.window.activeTextEditor;
   // textEditor is hooribly slow when updating text
   // while websockets will respond almost immediatly after the first word
