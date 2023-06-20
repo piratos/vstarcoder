b57da2f4ec97b17ad66444c87c515d0ae156b263
piratos <sayf.piratos@gmail.com>
2023-06-20 10:35:08 +0200

    Add support for fim
    update readme and screen cast

--- README.md
+++ README.md
@@ -32,12 +32,17 @@ PS: If you run the backend using the following step then use
 Example: Running using starcoder ct2fast version (for faster inference)
 
 ```
-python main.py --pretrained michaelfeil/ct2fast-starcoder
+python main.py --pretrained piratos/ct2fast-starcoderplus
 ```
 
 PS: the pretrained entry can be a local folder or a huggingface repo
 
 ## Use
+
+### 1 line inference
+
+This keybinding only sends the current line to the API, this is useful for
+simple function where the function naming is very obvious
 
 1. Place the cursor in the line you want to send to inference server
 2. Press the keycombination Ctrl + k + k (Press Ctrl then double press "k" button)
@@ -67,6 +72,71 @@ def find_children_processes(parent_pid):
 
 def main():
     children = find_children_processes(1)
+```
+
+### Context selection inference
+
+This is useful when you need to send multiple lines to the API to give more context
+
+1. Select the multiple lines containing the context to send to the API
+2. Press the keycombination Ctrl + l + l (Press Ctrl then double press "l" button)
+3. Completion will be added after the last line of the selection
+
+Example:
+
+Before pressing
+```
+
+<start selection># Remove file extension from files in a given folder
+# arguments: folder: string, extension: string
+# return: List of files renamed
+def remove_file_extension(folder, extension):<end selection>
+
+def main():
+    children = find_children_processes(1)
+```
+
+After pressing Ctrl + l + l
+
+```
+# Remove file extension from files in a given folder
+# arguments: folder: string, extension: string
+# return: List of files renamed
+def remove_file_extension(folder, extension):
+    files = os.listdir(folder)
+    files_to_rename = []
+    for file in files:
+        if file.endswith(extension):
+            files_to_rename.append(file)
+    for file in files_to_rename:
+        os.rename(os.path.join(folder, file), os.path.join(folder, file[:-len(extension)]))
+    return files_to_rename
+
+<Extra lines may appear if the model does not do early stopping (like starcoder)>
+
+def main():
+    children = find_children_processes(1)
+```
+
+### Fill in the middle inference
+
+This one is used if you need the model to fill code depending on what is before
+and after the current line.
+PS: You can control the number of lines sent as context (before and after) from
+the extension settings.
+
+Before pressing
+```
+def hello_world():
+<cursor is here, line will be overwritten>
+    print(hello_string)
+```
+
+After pressing
+```
+def hello_world():
+    hello_string = "Hello, World!"
+    print(hello_string)
 ```
 
 ## Why not using [huggingface-vscode](https://github.com/huggingface/huggingface-vscode)
--- extension/src/extension.ts
+++ extension/src/extension.ts
@@ -18,11 +18,18 @@ export function activate(context: vscode.ExtensionContext) {
     }
   });
 
+  let disposablecontext = vscode.commands.registerCommand('extension.sendContextToApi', async () => {
+    const editor = vscode.window.activeTextEditor;
+    if (editor) {
+      await sendContextToApi();
+    }
+  });
   context.subscriptions.push(disposableline);
   context.subscriptions.push(disposableselection);
+  context.subscriptions.push(disposablecontext);
 }
 
-async function insertTextAfterCurrentLine(text: string, after_selection: boolean = false) {
+async function insertTextAfterCurrentLine(text: string, overwrite: boolean = false, after_selection: boolean = false) {
   console.log(text);
   const editor = vscode.window.activeTextEditor;
   if (editor) {
@@ -36,16 +43,28 @@ async function insertTextAfterCurrentLine(text: string, after_selection: boolean
     }
     else {
       const line = editor.selection.active.line;
-      const lineText = editor.document.lineAt(line).text;
 
       await new Promise<void>((resolve) => {
         editor.edit((editBuilder) => {
-          editBuilder.insert(new vscode.Position(line + 1, 0), `${text}\n`);
+          if (overwrite) {
+            editBuilder.insert(new vscode.Position(line, 0), `${text}`);
+          }
+          else {
+            editBuilder.insert(new vscode.Position(line + 1, 0), `${text}\n`);
+          }
         }).then(() => {
           resolve();
         });
       });
-      const newPosition = new vscode.Position(line + 2, 0); // Move the cursor to the line after the pasted text
+
+      // Update cursor position
+      let newPosition: vscode.Position;
+      if (overwrite){
+        newPosition = new vscode.Position(line, 0);
+      }
+      else {
+        newPosition = new vscode.Position(line + 2, 0);
+      }
       const newSelection = new vscode.Selection(newPosition, newPosition);
       editor.selection = newSelection;
     }
@@ -55,8 +74,8 @@ async function insertTextAfterCurrentLine(text: string, after_selection: boolean
 
 
 async function sendLineToApi(line: string) {
-  const endpoint = vscode.workspace.getConfiguration().get('lineToApi.apiEndpoint') as string;
-  const maxtokens = vscode.workspace.getConfiguration().get('lineToApi.maxTokens') as number;
+  const endpoint = vscode.workspace.getConfiguration().get('vstarcoder.apiEndpoint') as string;
+  const maxtokens = vscode.workspace.getConfiguration().get('vstarcoder.maxTokens') as number;
 
   try {
     const data = {
@@ -80,8 +99,8 @@ async function sendLineToApi(line: string) {
 
 async function sendSelectionToApi() {
   const editor = vscode.window.activeTextEditor;
-  const endpoint = vscode.workspace.getConfiguration().get('lineToApi.apiEndpoint') as string;
-  const maxtokens = vscode.workspace.getConfiguration().get('lineToApi.maxTokens') as number;
+  const endpoint = vscode.workspace.getConfiguration().get('vstarcoder.apiEndpoint') as string;
+  const maxtokens = vscode.workspace.getConfiguration().get('vstarcoder.maxTokens') as number;
   if (editor) {
     const selection = editor.selection;
     const selectedText = editor.document.getText(selection);
@@ -97,7 +116,7 @@ async function sendSelectionToApi() {
 
         const generatedText = response.data.generated_text;
         if (generatedText) {
-          await insertTextAfterCurrentLine(generatedText, true);
+          await insertTextAfterCurrentLine(generatedText, false, true);
         } else {
           vscode.window.showErrorMessage('Failed to generate text from API.');
         }
@@ -106,6 +125,61 @@ async function sendSelectionToApi() {
       }
     } else {
       vscode.window.showErrorMessage('No text is selected.');
+    }
+  } else {
+    vscode.window.showErrorMessage('No active text editor.');
+  }
+}
+
+async function sendContextToApi() {
+  const editor = vscode.window.activeTextEditor;
+  const endpoint = vscode.workspace.getConfiguration().get('vstarcoder.apiEndpoint') as string;
+  const contextLines = vscode.workspace.getConfiguration().get('vstarcoder.contextSize') as number;
+  const maxtokens = vscode.workspace.getConfiguration().get('vstarcoder.maxTokens') as number;
+  if (editor) {
+    const document = editor.document;
+    const currentLine = editor.selection.active.line;
+
+    const startLine = Math.max(currentLine - contextLines, 0);
+    const endLine = Math.min(currentLine + contextLines, document.lineCount - 1);
+
+    const contextLinesText = [];
+    for (let line = startLine; line <= endLine; line++) {
+      if (line == startLine){
+        const lineText = "<fim_prefix>" + document.lineAt(line).text;
+        contextLinesText.push(lineText);
+      }
+      else if (line == endLine){
+        const lineText = document.lineAt(line).text + "<fim_middle>";
+        contextLinesText.push(lineText);
+      }
+      else if (line == currentLine){
+        // current line is ignored as it will be replaced
+        contextLinesText.push("<fim_suffix>")
+      }
+      else {
+        const lineText = document.lineAt(line).text;
+        contextLinesText.push(lineText);
+      }
+    }
+
+    const contextText = contextLinesText.join('\n');
+    try {
+      const response = await axios.post(endpoint, {
+        inputs: contextText,
+        parameters: {
+          max_new_tokens: maxtokens
+        }
+      });
+
+      const generatedText = response.data.generated_text;
+      if (generatedText) {
+        await insertTextAfterCurrentLine(generatedText, true, false);
+      } else {
+        vscode.window.showErrorMessage('Failed to generate text from API.');
+      }
+    } catch (error) {
+      vscode.window.showErrorMessage('An error occurred while calling the API.');
     }
   } else {
     vscode.window.showErrorMessage('No active text editor.');
